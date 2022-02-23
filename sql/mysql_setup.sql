6cd40f2ad7181a6490d9368a33e0fa56855a1b19
tomasz154 <tomasz154@10g.pl>
2022-02-23 22:22:16 +0100

    Create fancy example

--- sql/mysql_setup.sql
+++ sql/mysql_setup.sql
@@ -1,5 +1,17 @@
-create database dev;
+createdatabase dev;
 use dev;
-create table t(id int);
-insert into t values (1), (2), (3);
-alter table t add constraint primary key t_pk(id);
+
+create table people
+(
+    id         int,
+    name       text,
+    birth_date date
+);
+
+insert into people
+values (1, 'Brunon', '1801-02-03'),
+       (2, 'Cedryk', '1885-06-05'),
+       (3, 'Kleofas', '1890-11-23');
+
+alter table people
+    add constraint primary key t_pk(id);
--- sql/postgres_mysql_setup.sql
+++ sql/postgres_mysql_setup.sql
@@ -1,31 +1,27 @@
-create schema mysql_fdw;
-create extension mysql_fdw schema mysql_fdw;
-create server mysql foreign data wrapper mysql_fdw options (host 'mysql', port '3306');
-create user mapping for postgres server mysql options (username 'root', password 'root');
-create schema mysql;
+-- load extension first time after install
+CREATE EXTENSION mysql_fdw;
 
--- mysql startup takes approx 30 secs to initialize the database
--- in contrast postgres starts within second
--- we have to try import schema for one minute before raising an error
-do $$
-declare
-   v_steps  int := 12;
-   v_sleep  int := 5;
-   idx      int;
-begin
-   for idx in 1..v_steps
-   loop
-      begin
-         import foreign schema dev from server mysql into mysql;
-         raise info using message = 'schema "dev" succesfully imported';
-         exit;
-      exception
-         when others then
-            raise warning using message = 'mysql db is not initialized yet. sleeping for ' || v_sleep || ' seconds...';
-            perform pg_sleep(v_sleep);
-      end;
-   end loop;
-end;
-$$;
+-- create server object
+CREATE SERVER mysql_server
+    FOREIGN DATA WRAPPER mysql_fdw
+    OPTIONS (host 'mysql', port '3306');
 
+-- create user mapping
+CREATE USER MAPPING FOR postgres
+    SERVER mysql_server
+    OPTIONS (username 'root', password 'root');
 
+-- create foreign table
+CREATE FOREIGN TABLE mysql_people
+    (
+        id         int,
+        name       text,
+        birth_date date
+        )
+    SERVER mysql_server
+    OPTIONS (dbname 'dev', table_name 'people');
+
+CREATE MATERIALIZED VIEW people
+AS
+SELECT name, extract(year from now()) - extract(year from birth_date) age
+FROM mysql_people;
